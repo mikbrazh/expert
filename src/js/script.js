22c7d29a2d7daab83e8be953bee0f062cce2604a
Mikhail Brazhnik <mikbrazh@gmail.com>
2023-03-17 00:09:34 +0300

    did swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 (function () {
 
-  const carousel = document.querySelector('.carousel');
-  const carouselWrapper = document.querySelector('.carousel-wrapper');
-  const mediaQuery = window.matchMedia('(max-width: 1503.98px)');
+  // const carousel = document.querySelector('.carousel');
+  // const carouselWrapper = document.querySelector('.carousel-wrapper');
+  // const mediaQuery = window.matchMedia('(max-width: 1503.98px)');
 
   // if (mediaQuery.matches) {
   //   carousel.classList.toggle('swiper');
@@ -10,13 +10,25 @@
   // }
 
 
-function handleTabletChange(e) {
-  if (e.matches) {
-    carousel.classList.add('swiper');
-    carouselWrapper.classList.add('swiper-wrapper');
-  }
+// function handleTabletChange(e) {
+//   if (e.matches) {
+//     carousel.classList.add('swiper');
+//     carouselWrapper.classList.add('swiper-wrapper');
+//   }
+// }
+// mediaQuery.addListener(handleTabletChange);
+// handleTabletChange(mediaQuery);
+
+const mediaQueryList = window.matchMedia("screen and (min-width: 600px)");
+
+mediaQueryList.addEventListener('change', foo);
+foo(mediaQuery);
+
+function activateSwiper(mq) {
+  const carousel = document.querySelector('.carousel');
+  const carouselWrapper = document.querySelector('.carousel-wrapper');
+  const mediaQuery = window.matchMedia('(max-width: 1503.98px)');
+    document.body.style.backgroundColor = mq.matches ? '#f00' : '#060';
 }
-mediaQuery.addListener(handleTabletChange);
-handleTabletChange(mediaQuery);
 
 })();
