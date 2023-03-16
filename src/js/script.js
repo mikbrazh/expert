00ef92a32382f39209c794cb84bd222e930113ce
Mikhail Brazhnik <mikbrazh@gmail.com>
2023-03-16 23:48:17 +0300

    did swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,22 @@
 (function () {
 
-  console.log(1);
+  const carousel = document.querySelector('.carousel');
+  const carouselWrapper = document.querySelector('.carousel-wrapper');
+  const mediaQuery = window.matchMedia('(max-width: 1503.98px)');
+
+  // if (mediaQuery.matches) {
+  //   carousel.classList.toggle('swiper');
+  //   carouselWrapper.classList.toggle('swiper-wrapper');
+  // }
+
+
+function handleTabletChange(e) {
+  if (e.matches) {
+    carousel.classList.add('swiper');
+    carouselWrapper.classList.add('swiper-wrapper');
+  }
+}
+mediaQuery.addListener(handleTabletChange);
+handleTabletChange(mediaQuery);
 
 })();
